03752557335ecd3c04dfca948a3d03042742e510
Alessandro Cristalli <alessandro.cristalli@hotmail.it>
2022-03-17 10:14:16 +0100

    concatenateRouteCitta

--- Server/controllers/cittaController.js
+++ Server/controllers/cittaController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const db = require('../db');
+const { query } = require('../db');
 
 
 //Per la ricerca di tutte le città 
@@ -11,9 +12,9 @@ app.get('/citta', (req, res) => {
   db.query(sql).then(result => {  //mettere controllo sugli errori
 
     res.json(result);
-      
+
   });
-}); 
+});
 
 
 
@@ -24,32 +25,34 @@ app.get('/citta/:id', (req, res) => {
   db.query(sql).then(result => {  //mettere controllo sugli errori
 
     res.json(result);
-      
+
   });
 
 })
+
 
 app.get('/:cNome', (req, res) => {
-  let sql = `SELECT * FROM citta WHERE cNome = '${req.params.cNome}' `;
+  const campo = req.query.campo;
+  const cNome = req.params.cNome;
+  if (typeof (campo) == 'undefined') {
+    const sql = `SELECT * FROM citta WHERE cNome = '${cNome}' `;
 
-  db.query(sql).then(result => {  //mettere controllo sugli errori
+    db.query(sql).then(result => {  //mettere controllo sugli errori
 
-    res.json(result);
-      
-  });
+      res.json(result);
 
-})
+    });
+  } else {
+    const sql =
+      `SELECT citta.id, campo.id, campo.nome, citta.cNome 
+      FROM campo 
+      INNER JOIN citta ON campo.id_citta = citta.id 
+      WHERE nome = '${campo}' and citta.cNome='${cNome}'`;
 
-
-app.get('/:cNome/?campo=:nome', (req, res) => {
-  let sql = `SELECT citta.id, campo.id, campo.nome, citta.cNome FROM campo INNER JOIN citta ON campo.id_citta = citta.id WHERE nome = '${req.params.nome}'`;
-
-  db.query(sql).then(result => {  //mettere controllo sugli errori
-
-    res.json(result);
-      
-  });
-
+    db.query(sql).then(result => {  //mettere controllo sugli errori
+      res.json(result);
+    })
+  }
 })
 
 module.exports = app;
